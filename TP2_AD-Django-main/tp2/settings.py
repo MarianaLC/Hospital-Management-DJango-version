a8b4ae9ba1ccd2d7c953a311d01304b388a0e1f7
Mariana Lindo <85584806+MarianaLC@users.noreply.github.com>
2022-09-05 20:27:33 +0100

    Update settings.py

--- TP2_AD-Django-main/tp2/settings.py
+++ TP2_AD-Django-main/tp2/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7&9iqiivox!1p-%etjg-^29fn6n1c=v#jwb+d0-qe=ye@aph^@'
+SECRET_KEY = 'xxxx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
